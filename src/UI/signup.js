c899b7617cef9320a5fcb31c7e8a3622651a9060
Vivek Verma <vivekverma74888@gmail.com>
2021-07-11 14:10:12 +0530

    signUp handeling

--- src/UI/signup.js
+++ src/UI/signup.js
@@ -8,7 +8,7 @@ function SignUp(){
 
     const SignUpFetch = async () =>{
         try{
-            console.log("fetch")
+            // console.log("fetch")
             const response = await fetch("http://localhost:3000/users/",{
                 method:"POST",
                 body: JSON.stringify({userId:signUpInfo.id, password:signUpInfo.password,firstName:signUpInfo.fname, lastName:signUpInfo.lname,state:signUpInfo.state, country:signUpInfo.country, city: signUpInfo.city, pinCode: signUpInfo.pincode}),
@@ -19,42 +19,67 @@ function SignUp(){
             });
             try{
                 const data = await response.json();
-                console.log(data);
+                console.log("t dta",data);
             }catch(err){
-                console.log(err);
+                console.log("c1 e",err);
             }
         }
         catch(error){
-            alert(error)
+            alert("c2 e",error)
         }
     };
     const signupHandeler = () =>{
         SignUpFetch();
     };
-        
 
+    const idHandeler = (e) => {
+        console.log("as")
+       setSignUpInfo({...signUpInfo, id:e.target.value}) 
+    };
+    const passwordHandeler = (e) => {
+        setSignUpInfo({...signUpInfo, password:e.target.value}) 
+    };
+    const fnameHandeler = (e) => {
+        setSignUpInfo({...signUpInfo, fname:e.target.value}) 
+    };
+    const lnameHandeler = (e) => {
+        setSignUpInfo({...signUpInfo, lname:e.target.value}) 
+    };
+    const stateHandeler = (e) => {
+        setSignUpInfo({...signUpInfo, state:e.target.value}) 
+    };
+    const countryHandeler = (e) => {
+        setSignUpInfo({...signUpInfo, country:e.target.value}) 
+    };
+    const cityHandeler = (e) => {
+        setSignUpInfo({...signUpInfo, city:e.target.value}) 
+    };
+    const pinCodeHandeler = (e) => {
+        setSignUpInfo({...signUpInfo, pincode:e.target.value})  
+    };
+    
 
 
     return(
         <Card>
             <div className='row'>
             <label className='form-label'>User Id</label>
-            <input className='form-control col-sm-6' onChange={(e) => setSignUpInfo({...signUpInfo, id:e.target.value})} type="text" ></input>
+            <input className='form-control col-sm-6' onChange={idHandeler} type="text" ></input>
             <label className='form-label '>Password</label>
-            <input className='form-control col-sm-6' onChange={(e) => setSignUpInfo({...signUpInfo, password:e.target.value})} type="text" ></input>
+            <input className='form-control col-sm-6' onChange={passwordHandeler} type="text" ></input>
             <label className='form-label'>First Name</label>
-            <input className='form-control col-sm-6' onChange={(e) => setSignUpInfo({...signUpInfo, fname:e.target.value})} type="text" ></input>
+            <input className='form-control col-sm-6' onChange={fnameHandeler} type="text" ></input>
             <label className='form-label'>lastName</label>
-            <input className='form-control col-sm-6' onChange={(e) => setSignUpInfo({...signUpInfo, lname:e.target.value})} type="text" ></input>
+            <input className='form-control col-sm-6' onChange={lnameHandeler} type="text" ></input>
             <label className='form-label'>State</label>
-            <input className='form-control col-sm-6' onChange={(e) => setSignUpInfo({...signUpInfo, state:e.target.value})} type="text" ></input>
+            <input className='form-control col-sm-6' onChange={stateHandeler} type="text" ></input>
             <label className='form-label'>Country</label>
-            <input className='form-control col-sm-6' onChange={(e) => setSignUpInfo({...signUpInfo, country:e.target.value})} type="text" ></input>
+            <input className='form-control col-sm-6' onChange={countryHandeler} type="text" ></input>
             <label className='form-label'>City</label>
-            <input className='form-control col-sm-6' onChange={(e) => setSignUpInfo({...signUpInfo, city:e.target.value})} type="text" ></input>
+            <input className='form-control col-sm-6' onChange={cityHandeler} type="text" ></input>
             <label className='form-label'>Pin Code</label>
-            <input className='form-control col-sm-6' onChange={(e) => setSignUpInfo({...signUpInfo, pincode:e.target.value})} type="number" ></input>
-            <Button onClick={signupHandeler}>submit</Button>
+            <input className='form-control col-sm-6' onChange={pinCodeHandeler} type="number" ></input>
+            <Button onClick={signupHandeler}>Create User</Button>
             </div>
         </Card>
     );
